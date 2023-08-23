3da8c79d6b9c01f169777515f2c096097d114c25
root <akhilsai2511@gmail.com>
2023-08-23 18:26:29 +0000

    commit message

--- cal.js
+++
@@ -1,6 +0,0 @@
-const add = (a, b) => {
-  return a + b;
-};
-const sub = (a, b) => a - b;
-exports.add = add;
-exports.sub = sub;
--- cal.mjs
+++
@@ -1,4 +0,0 @@
-export const add = (a, b) => {
-  return a + b;
-};
-export const sub=(a,b) => a-b;
